66970814144c953231bac92b5c441fd7ffd0bdcc
ENZZO FERNANDES DAS CHAGAS <25203668@CFP127ALUNO.LOCAL>
2026-03-16 17:03:12 -0300

    Iniciando atividade escolha-animal

---
+++ 05-escolha-animal/app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function definirEscolha(){
+    const escolha = getElementbyId('select').value
+    if (escolha == 'Vertebrado'){
+        vertebrado()
+    }else{
+        invertebrado()
+    }
+}
+
+function vertebrado(){
+
+}
+
+function invertebrado(){
+
+}
